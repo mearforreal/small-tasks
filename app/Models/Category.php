6e8719d5cff2cb2ad2c6e5d8bb8f63e3725a7b8d
mearforreal <meirerbol@gmail.com>
2023-10-09 21:00:49 +0600

    category sub category recusive

---
+++ app/Models/Category.php
@@ -0,0 +1,26 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\Relations\HasMany;
+
+class Category extends Model
+{
+    use HasFactory;
+
+    protected $fillable = [
+        'title', 'category_id'
+    ];
+
+    public function categories(): HasMany
+    {
+        return $this->hasMany(Category::class);
+    }
+
+    public function childrenCategories()
+    {
+        return $this->hasMany(Category::class)->with('categories');
+    }
+}
--- database/seeders/CategorySeeder.php
+++ database/seeders/CategorySeeder.php
@@ -2,6 +2,7 @@
 
 namespace Database\Seeders;
 
+use App\Models\Category;
 use Illuminate\Database\Console\Seeds\WithoutModelEvents;
 use Illuminate\Database\Seeder;
 
@@ -12,6 +13,20 @@ class CategorySeeder extends Seeder
      */
     public function run(): void
     {
-        //
+        // Create root categories
+        $category1 = Category::create(['title' => 'Техническая поддержка']);
+        $category2 = Category::create(['title' => 'Отдел продаж']);
+
+        // Create subcategories for Root Category 1
+        $subcategory1_1 = $category1->childrenCategories()->create(['title' => 'Сервер']);
+        $subcategory1_2 = $category1->childrenCategories()->create(['title' => 'Аппаратное обеспечение']);
+
+        // // Create sub-subcategories for Subcategory 1.1
+        // $subSubcategory1_1_1 = $subcategory1_1->children()->create(['name' => 'Sub-subcategory 1.1.1']);
+        // $subSubcategory1_1_2 = $subcategory1_1->children()->create(['name' => 'Sub-subcategory 1.1.2']);
+
+        // Create subcategories for Root Category 2
+        $subcategory2_1 = $category2->childrenCategories()->create(['title' => 'Возврат']);
+        $subcategory2_2 = $category2->childrenCategories()->create(['title' => 'Услуги']);
     }
 }
